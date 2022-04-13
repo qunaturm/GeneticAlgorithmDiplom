bfc26d1dad69766d4eee3fc85c9df84407b2bac6
Karina Khismatulina <hkarinaf@gmail.com>
2022-04-13 22:21:07 +0300

    fix: fix crossing methods

--- GeneticAlgorithmDiplom/Genitor/Crossing/Two_Point_Crossover.cs
+++ GeneticAlgorithmDiplom/Genitor/Crossing/Two_Point_Crossover.cs
@@ -2,23 +2,20 @@
 {
     public static class Two_Point_Crossover
     {
-        public static Func<List<Individual>, List<Individual>> Crossover = (parents) =>
+        public static Func<List<Individual>, Individual> Crossover = (parents) =>
         {
-            List<Individual> children = new List<Individual>();
             var random = new Random();
             var firstParent = parents[0];
             var secondParent = parents[1];
 
             //get indexes of chromosome for children
-            var firstHalf = random.Next(1, firstParent.matrix.Length - 2);
+            var firstHalf = random.Next(1, firstParent.Matrix.Length - 2);
             var secondHalf = 0;
-            secondHalf = firstHalf < firstParent.matrix.Length ? secondHalf = random.Next(firstHalf + 1, firstParent.matrix.Length - 2) : secondHalf = random.Next(1, firstHalf);
+            secondHalf = firstHalf < firstParent.Matrix.Length ? secondHalf = random.Next(firstHalf + 1, firstParent.Matrix.Length - 2) : secondHalf = random.Next(1, firstHalf);
 
-            var listTwoChildren = MatrixOperations.CopyColumn(firstParent.matrix, secondParent.matrix, firstHalf, secondHalf);
+            var listTwoChildren = MatrixOperations.CopyColumn(firstParent.Matrix, secondParent.Matrix, firstHalf, secondHalf);
 
-            children.Add(listTwoChildren[0]);
-            children.Add(listTwoChildren[1]);
-            return children;
+            return listTwoChildren[0];
         };
     }
 }

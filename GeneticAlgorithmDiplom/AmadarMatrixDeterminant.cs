67c0dc7c1b516bf9ed76609e71e4d4520100b701
Karina Khismatulina <hkarinaf@gmail.com>
2022-04-18 13:55:12 +0300

    feat: add hamadar matrixes values

---
+++ GeneticAlgorithmDiplom/AmadarMatrixDeterminant.cs
@@ -0,0 +1,27 @@
+﻿namespace GeneticAlgorithmDiplom
+{
+    public class AmadarMatrixDeterminant
+    {
+        public static int CheckWithAmadar(int dimention)
+        {
+            switch (dimention)
+            {
+                case 1: return 1;
+                case 2: return 2;
+                case 3: return 4;
+                case 4: return 16;
+                case 5: return 48;
+                case 6: return 160;
+                case 7: return 576;
+                case 8: return 4096;
+                case 9: return 14336;
+                case 10: return 73728;
+                case 11: return 327680;
+                case 12: return 2985984;
+                case 13: return 14929920;
+                default: break;
+            }
+            throw new ArgumentException($"Determinant for{dimention} dimention doesn't found for now");
+        }
+    }
+}
